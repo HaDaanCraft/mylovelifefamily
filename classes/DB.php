3d88d2a3b6f0e36f019b7005bd1e2cab90eba937
HaDaanCraft <hermans.daan@hotmail.com>
2018-07-17 17:28:28 +0200

    Test

--- classes/DB.php
+++ classes/DB.php
@@ -2,7 +2,7 @@
 class DB {
 
         private static function connect() {
-          $pdo = new PDO('mysql:host=127.0.0.1;dbname=mylovelifefamily;charset=utf8', 'root', '');
+          $pdo = new PDO('mysql:host=127.0.0.1;dbname=mylovelifefamily;charset=utf8', 'root', 'Daan0109');
           $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           return $pdo;
         }
