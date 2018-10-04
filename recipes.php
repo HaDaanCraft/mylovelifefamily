82886dfa33cb46fa8e9b2d00aa7b394cabaf2eb4
HaDaanCraft <hermans.daan@hotmail.com>
2018-10-04 21:32:00 +0200

    Added recipes page

---
+++ recipes.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>MyLoveLifeFamily</title>
+    <link rel="icon" href="./assets/pictures/family.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="js/main.js"></script>
+  </head>
+  <body>
+
+    <div class="navResponsive" id="navResponsive">
+    </div>
+
+    <div class="nav" id="nav">
+    </div>
+
+    <div class="recipes">
+      <div class="recipesWrapper">
+        <h3>Gerechten</h3>
+        <div class="recipeDiv">
+          <div class="recipe">
+            <img src="./assets/pictures/recipes/recipe1.jpg" />
+            <hr />
+            <p class="text"><underline>Ingrediënten</underline></p>
+            <ul class="left">
+              <li>1 Aubergine</li>
+              <li>1 Courgette</li>
+              <li>1 Rode paprika</li>
+              <li>1 Ui</li>
+              <li>1 Teen knoflook</li>
+            </ul>
+            <ul class="right">
+              <li>2 dl Tomatensaus</li>
+              <li>4 Stukken zeeduivel</li>
+              <li>4 Eetlepels olijfolie</li>
+              <li>Peper en zout</li>
+              <li>1 Kruidentuiltje met tijm, laurier en peterselie</li>
+            </ul>
+            <p class="text"><underline>Recept</underline></p>
+            <p class="recept"></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
