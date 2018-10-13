2e5127706f5eb8d88a24be1a00f273a56497d6b0
HaDaanCraft <hermans.daan@hotmail.com>
2018-10-13 17:03:46 +0200

    Added viewrecipe

---
+++ viewrecipe.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>MyLoveLifeFamily</title>
+    <link rel="icon" href="./assets/pictures/family.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="js/main.js"></script>
+  </head>
+  <body>
+
+    <div class="navResponsive" id="navResponsive">
+    </div>
+
+    <div class="nav" id="nav">
+    </div>
+
+    <div class="recipes">
+      <div class="recipesWrapper">
+        <a href="recipes.php"><img src="./assets/pictures/back.png" alt="Terug" id="back"></a>
+      </div>
+    </div>
+
+  </body>
+</html>
