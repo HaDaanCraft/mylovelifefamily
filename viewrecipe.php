b1d1cf40de6381d3edccf947628749c14b654dc3
HaDaanCraft <hermans.daan@hotmail.com>
2018-10-13 22:10:25 +0200

    Finished recipes for laptop view

--- viewrecipe.php
+++ viewrecipe.php
@@ -1,3 +1,10 @@
+<?php
+include('private/DB.php');
+
+$recipe = DB::query('SELECT * FROM recipes WHERE id=:id', array('id'=>$_GET['id']))[0];
+
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -22,6 +29,20 @@
     <div class="recipes">
       <div class="recipesWrapper">
         <a href="recipes.php"><img src="./assets/pictures/back.png" alt="Terug" id="back"></a>
+        <h3><?php echo $recipe['name'];?></h3>
+        <div class="recipeDiv">
+          <div class="photo">
+            <img src="<?php echo $recipe['photo']; ?>" />
+          </div>
+          <div class="ingredients">
+            <p class="hltext"><underline>Ingediënten</underline></p>
+            <?php echo $recipe['ingredients']; ?>
+          </div>
+          <div class="preparation">
+            <p class="hltext"><underline>Recept</underline></p>
+            <?php echo $recipe['recipe']; ?>
+          </div>
+        </div>
       </div>
     </div>
 
