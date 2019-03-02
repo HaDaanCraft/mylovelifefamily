9622b21485bbd6205477424680556a0dc958f31a
HaDaanCraft <hermans.daan@hotmail.com>
2019-03-02 20:02:47 +0100

    Beautify require.js and added comment to js cookie include at index

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <meta charset="utf-8">
     <title>MyLoveLifeFamily</title>
     <link rel="icon" href="./assets/pictures/family.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <!-- https://github.com/js-cookie/js-cookie -->
     <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <script src="js/main.js"></script>
-  </head>
-  <body>
+</head>
+
+<body>
 
     <div class="navResponsive" id="navResponsive">
     </div>
@@ -20,22 +23,23 @@
     </div>
 
     <div class="timeline" id="timeline">
-      <div class="timelineWrapper">
-        <h3><a href="#timeline">Tijdlijn</a></h3>
-        <div class="timelinegrid">
-          <p>Nieuwpoort - 2018</p>
-          <img src="./assets/pictures/nieuwpoort.jpg">
-          <img src="./assets/pictures/family_pic.jpg">
-          <p>Zeeland - 2018</p>
-          <p>Welkom Tuur in de familie - 11/01/2018</p>
-          <img src="./assets/pictures/tuur.jpg">
-          <img src="./assets/pictures/verjaardag-marie-2017.jpg">
-          <p>Verjaardag Marie - 2017</p>
-          <p>Verjaardag Eline - 2016</p>
-          <img src="./assets/pictures/verjaardag-eline-2016.jpg">
+        <div class="timelineWrapper">
+            <h3><a href="#timeline">Tijdlijn</a></h3>
+            <div class="timelinegrid">
+                <p>Nieuwpoort - 2018</p>
+                <img src="./assets/pictures/nieuwpoort.jpg">
+                <img src="./assets/pictures/family_pic.jpg">
+                <p>Zeeland - 2018</p>
+                <p>Welkom Tuur in de familie - 11/01/2018</p>
+                <img src="./assets/pictures/tuur.jpg">
+                <img src="./assets/pictures/verjaardag-marie-2017.jpg">
+                <p>Verjaardag Marie - 2017</p>
+                <p>Verjaardag Eline - 2016</p>
+                <img src="./assets/pictures/verjaardag-eline-2016.jpg">
+            </div>
         </div>
-      </div>
     </div>
 
-  </body>
-</html>
+</body>
+
+</html> 
