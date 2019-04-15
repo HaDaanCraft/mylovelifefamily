f20fe4f9e99c63f72c049984667f8b9088332e5a
HaDaanCraft <hermans.daan@hotmail.com>
2019-04-15 11:31:11 +0200

    Test

--- photoalbum.php
+++ photoalbum.php
@@ -1,36 +1,35 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8">
+    <title>MyLoveLifeFamily</title>
+    <link rel="icon" href="./assets/pictures/family.ico">
+    <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="js/main.js"></script>
+    <link href="https://unpkg.com/nanogallery2/dist/css/nanogallery2.min.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript" src="https://unpkg.com/nanogallery2/dist/jquery.nanogallery2.min.js"></script>
+  </head>
+  <body>
 
-<head>
-  <meta charset="utf-8">
-  <title>MyLoveLifeFamily</title>
-  <link rel="icon" href="./assets/pictures/family.ico">
-  <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1">
-  <link rel="stylesheet" type="text/css" href="css/main.css">
-  <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
-  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="js/main.js"></script>
-  <link href="https://raw.githubusercontent.com/nanostudio-org/nanogallery2/master/dist/css/nanogallery2.min.css" rel="stylesheet" type="text/css">
-  <script type="text/javascript" src="https://raw.githubusercontent.com/nanostudio-org/nanogallery2/master/dist/jquery.nanogallery2.min.js"></script>
-</head>
+    <div class="navResponsive" id="navResponsive">
+    </div>
 
-<body>
+    <div class="nav" id="nav">
+    </div>
 
-  <div class="navResponsive" id="navResponsive">
-  </div>
-
-  <div class="nav" id="nav">
-  </div>
-
-  <div data-nanogallery2='{
+    <div data-nanogallery2='{
         "userID": "108666537171504081916",
         "kind": "google2",
         "google2URL": "http://myhappyfamily.ddns.net:8080/mylovelifefamily/nanogp2/nanogp2.php",
-        "thumbnailWidth": "200"
+        "whiteList": "MyLoveLifeFamily",
+        "thumbnailWidth": "200",
+        "thumbnailAlignment": "center"
       }'>
 
-  </div>
+    </div>
 
-</body>
-
+  </body>
 </html>
