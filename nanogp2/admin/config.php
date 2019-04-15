c9df79bcd6b536abfa69ce513a05fde0bc54bc63
HaDaanCraft <hermans.daan@hotmail.com>
2019-04-15 12:01:52 +0200

    Added nano_pictures and pictures

--- nanogp2/admin/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$cfg_client_id          = '192607877764-6rn172ktbvls1v9rrdv4ubm7v2b8totk.apps.googleusercontent.com';
-$cfg_client_secret      = 'zGLE01tcVvAF_ddJstJVNthf';
-$cfg_max_accounts       = 1;
-$cfg_log                = false;
-$albums_filter          = ['sauvegarde', 'backup'];
-?>
