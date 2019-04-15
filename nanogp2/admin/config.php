7b11a606791925eaf1e93f39e9a4275e4da259c0
HaDaanCraft <hermans.daan@hotmail.com>
2019-04-15 10:57:11 +0200

    Added nanogp2

---
+++ nanogp2/admin/config.php
@@ -0,0 +1,7 @@
+<?php
+$cfg_client_id          = 'your_client_id';
+$cfg_client_secret      = 'your_client_secret';
+$cfg_max_accounts       = 1;
+$cfg_log                = false;
+$albums_filter          = ['sauvegarde', 'backup'];
+?>
