437e5dfec6c6e46c65d0ef8fac06ba582b6325ab
HaDaanCraft <hermans.daan@hotmail.com>
2019-04-15 11:11:26 +0200

    updated config

--- nanogp2/admin/config.php
+++ nanogp2/admin/config.php
@@ -1,6 +1,6 @@
 <?php
-$cfg_client_id          = 'your_client_id';
-$cfg_client_secret      = 'your_client_secret';
+$cfg_client_id          = '192607877764-6rn172ktbvls1v9rrdv4ubm7v2b8totk.apps.googleusercontent.com';
+$cfg_client_secret      = 'zGLE01tcVvAF_ddJstJVNthf';
 $cfg_max_accounts       = 1;
 $cfg_log                = false;
 $albums_filter          = ['sauvegarde', 'backup'];
